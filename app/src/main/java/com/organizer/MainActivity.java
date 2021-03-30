99ab9cb067884953f2675737005e9a740c20679c
Maxi Pulstorm <maxipulstorm@aon.at>
2021-03-30 19:32:30 +0200

    implement MainLayout swipe

--- app/src/main/java/com/organizer/MainActivity.java
+++ app/src/main/java/com/organizer/MainActivity.java
@@ -4,15 +4,18 @@ import java.util.Objects;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.organizer.layouts.MainLayout;
 import com.organizer.layouts.calendar.CalendarLayout;
 import com.organizer.layouts.todo.TasksLayout;
 import static com.organizer.Functions.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         final Context context = this;
         Objects.requireNonNull(this.getSupportActionBar()).hide();
@@ -23,16 +26,16 @@ public class MainActivity extends AppCompatActivity {
         actionBarSize = dpToPx(context, context.getResources().getInteger(R.integer.actionBarSize));
         MainLayout mainLayout = new MainLayout(context);
         setContentView(mainLayout);
-
-        new Task(context, buildId(todayD,todayM,todayY), "task", 8, 12);
-        new Task(context, buildId(todayD+1,todayM,todayY), "task", 6, 15);
-        new Task(context, buildId(todayD+1,todayM,todayY), "task", 23, 24);
-        new Task(context, buildId(todayD+2,todayM,todayY), "task", 6, 17);
-        new Task(context, buildId(todayD+2,todayM,todayY), "task", 17, 18);
-
+        
+        new Task(context, buildId(todayD, todayM, todayY), "task", 8, 12);
+        new Task(context, buildId(todayD + 1, todayM, todayY), "task", 6, 15);
+        new Task(context, buildId(todayD + 1, todayM, todayY), "task", 23, 24);
+        new Task(context, buildId(todayD + 2, todayM, todayY), "task", 6, 17);
+        new Task(context, buildId(todayD + 2, todayM, todayY), "task", 17, 18);
+        
         CalendarLayout calendarLayout = new CalendarLayout(context);
         mainLayout.addView(calendarLayout);
         TasksLayout tasksLayout = new TasksLayout(context);
-        mainLayout.addView(tasksLayout);
+        mainLayout.addView(new Button(context));
     }
 }
--- app/src/main/java/com/organizer/layouts/MainLayout.java
+++ app/src/main/java/com/organizer/layouts/MainLayout.java
@@ -6,37 +6,43 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
-import android.widget.Toast;
 import com.organizer.R;
 
-public class MainLayout extends ViewGroup {
+public class MainLayout extends ViewGroup
+{
     Context context;
     GestureDetector gestureDetector = null;
     boolean init = true;
     private int startingChild = 0;
     private int currentChild = startingChild;
-    View.OnTouchListener touchListener = new View.OnTouchListener() {
+    View.OnTouchListener touchListener = new View.OnTouchListener()
+    {
         @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
+        public boolean onTouch(View view, MotionEvent motionEvent)
+        {
             return gestureDetector.onTouchEvent(motionEvent);
         }
     };
-
-    public MainLayout(Context context) {
+    
+    public MainLayout(Context context)
+    {
         super(context);
         this.context = context;
         setGestureListener();
     }
-
-    public MainLayout(Context context, AttributeSet attributes) {
+    
+    public MainLayout(Context context, AttributeSet attributes)
+    {
         this(context, attributes, 0);
         this.context = context;
         setGestureListener();
     }
-
-    public MainLayout(Context context, AttributeSet attributes, int defStyle) {
+    
+    public MainLayout(Context context, AttributeSet attributes, int defStyle)
+    {
         super(context, attributes, defStyle);
         this.context = context;
         setGestureListener();
@@ -48,90 +54,136 @@ public class MainLayout extends ViewGroup {
         return false;
     }
     */
-
+    
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        if (init) {
-            for (int i = 0; i < getChildCount(); i++) {
+    protected void onLayout(boolean changed, int l, int t, int r, int b)
+    {
+        if (init)
+        {
+            for (int i = 0; i < getChildCount(); i++)
+            {
                 getChildAt(i).layout(l, t, r, b);
                 getChildAt(i).setOnTouchListener(touchListener);
-                getChildAt(i).setVisibility(View.GONE);
+                getChildAt(i).setVisibility(View.INVISIBLE);
             }
-            if (startingChild < 0 || startingChild >= getChildCount()) {
+            if (startingChild < 0 || startingChild >= getChildCount())
+            {
                 startingChild = 0;
             }
             getChildAt(startingChild).setVisibility(View.VISIBLE);
-            getChildAt(0).setVisibility(View.VISIBLE);
             init = false;
         }
         //Toast.makeText(context, "onLayout", Toast.LENGTH_SHORT).show();
     }
-
-    private void setGestureListener() {
-        gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+    
+    private void setGestureListener()
+    {
+        gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener()
+        {
             private static final int SWIPE_THRESHOLD = 100;
             private static final int SWIPE_VELOCITY_THRESHOLD = 100;
-
+            
             @Override
-            public boolean onDown(MotionEvent e) {
+            public boolean onDown(MotionEvent e)
+            {
                 return true;
             }
-
+            
             @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+            {
                 boolean result = false;
-                try {
+                try
+                {
                     float diffY = e2.getY() - e1.getY();
                     float diffX = e2.getX() - e1.getX();
-                    if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD && Math.abs(diffY) > Math.abs(diffX)) {
-                        if (diffY > 0) {
+                    if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD && Math.abs(diffY) > Math.abs(diffX))
+                    {
+                        if (diffY > 0)
+                        {
                             onSwipeDown();
-                        } else {
+                        }
+                        else
+                        {
                             onSwipeUp();
                         }
                         result = true;
                     }
-                } catch (Exception exception) {
+                }
+                catch (Exception exception)
+                {
                     exception.printStackTrace();
                 }
                 return result;
             }
-
-            void onSwipeUp() {
-                if (currentChild < getChildCount() - 1) {
-                    AnimationSet animationSet;
-
-                    animationSet = new AnimationSet(false);
-                    animationSet.addAnimation(AnimationUtils.loadAnimation(context, R.anim.fadeoutup));
-                    getChildAt(currentChild).startAnimation(animationSet);
-
+            
+            void onSwipeUp()
+            {
+                if (currentChild < getChildCount() - 1)
+                {
+                    AnimationSet animation = new AnimationSet(false);
+                    final View child1 = getChildAt(currentChild);
+                    final View child2 = getChildAt(currentChild + 1);
+                    
+                    animation.addAnimation(AnimationUtils.loadAnimation(context, R.anim.fadeoutup));
+                    animation.setAnimationListener(new Animation.AnimationListener()
+                    {
+                        @Override
+                        public void onAnimationStart(Animation animation)
+                        {
+                            child1.setVisibility(View.INVISIBLE);
+                        }
+                        
+                        @Override
+                        public void onAnimationEnd(Animation animation)
+                        {
+                            child2.setVisibility(View.VISIBLE);
+                        }
+                        
+                        @Override
+                        public void onAnimationRepeat(Animation animation)
+                        {
+                        
+                        }
+                    });
+                    child1.startAnimation(animation);
+                    
                     currentChild++;
-                    getChildAt(currentChild).setVisibility(View.VISIBLE);
-                    animationSet = new AnimationSet(false);
-                    animationSet.addAnimation(AnimationUtils.loadAnimation(context, R.anim.fadeinup));
-                    getChildAt(currentChild).startAnimation(animationSet);
-                    getChildAt(currentChild - 1).setVisibility(View.GONE);
-                } else {
-                    Toast.makeText(context, "too little elements", Toast.LENGTH_SHORT).show();
                 }
             }
-
-            void onSwipeDown() {
-                if (currentChild > 0) {
-                    AnimationSet animationSet;
-
-                    animationSet = new AnimationSet(false);
-                    animationSet.addAnimation(AnimationUtils.loadAnimation(context, R.anim.fadeoutdown));
-                    getChildAt(currentChild).startAnimation(animationSet);
-                    getChildAt(currentChild).setVisibility(View.GONE);
-
+            
+            void onSwipeDown()
+            {
+                if (currentChild > 0)
+                {
+                    AnimationSet animation = new AnimationSet(false);
+                    final View child1 = getChildAt(currentChild);
+                    final View child2 = getChildAt(currentChild - 1);
+                    
+                    animation.addAnimation(AnimationUtils.loadAnimation(context, R.anim.fadeoutdown));
+                    animation.setAnimationListener(new Animation.AnimationListener()
+                    {
+                        @Override
+                        public void onAnimationStart(Animation animation)
+                        {
+                            child1.setVisibility(View.INVISIBLE);
+                        }
+                        
+                        @Override
+                        public void onAnimationEnd(Animation animation)
+                        {
+                            child2.setVisibility(View.VISIBLE);
+                        }
+                        
+                        @Override
+                        public void onAnimationRepeat(Animation animation)
+                        {
+                        
+                        }
+                    });
+                    child1.startAnimation(animation);
+                    
                     currentChild--;
-                    getChildAt(currentChild).setVisibility(View.VISIBLE);
-                    animationSet = new AnimationSet(false);
-                    animationSet.addAnimation(AnimationUtils.loadAnimation(context, R.anim.fadeindown));
-                    getChildAt(currentChild).startAnimation(animationSet);
-                } else {
-                    Toast.makeText(context, "arrived at top", Toast.LENGTH_SHORT).show();
                 }
             }
         });
