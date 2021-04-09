a135a7f08aafb9a235eb6fcdf8482c7745407570
duuwhang <maxipulstorm@aon.at>
2021-04-09 20:15:46 +0200

    implement waterfall layout placement

--- app/src/main/java/com/organizer/layouts/todo/ToDoLayout.java
+++ app/src/main/java/com/organizer/layouts/todo/ToDoLayout.java
@@ -1,36 +1,62 @@
 package com.organizer.layouts.todo;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.view.View;
 import android.widget.Button;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import com.organizer.MainActivity;
 import com.organizer.layouts.BaseLayout;
 
 public class ToDoLayout extends BaseLayout
 {
-    private List<ToDoElement> elements = new ArrayList<>();
+    private List<String> elements = new ArrayList<>();
+    private final Rect childRect = new Rect();
     
     public ToDoLayout(Context context)
     {
         super(context);
-        for (int i = 0; i < 10; i++)
+        
+        elements.addAll(Arrays.asList("Task1", "Task2", "Task3", "Task4", "Task5", "Task6", "Task7"));
+        
+        for (int i = 0; i < elements.size(); i++)
         {
-            Button b = new Button(context);
-            b.setText(Integer.toString(i));
-            addView(b);
+            Button button = new Button(context);
+            button.setText(elements.get(i));
+            button.setWidth(new Random().nextInt(500) + 500);
+            addView(button);
         }
         
         setLayoutParams(new LayoutParams(
-            getChildCount() * 300,
+            3000,
             LayoutParams.MATCH_PARENT));
     }
     
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom)
     {
+        int[] rowWidths = new int[4];
         for (int i = 0; i < getChildCount(); i++)
         {
-            getChildAt(i).layout(left + (300 * i), top, left + (300 * i) + 300, bottom);
+            View child = getChildAt(i);
+            int minWidthIndex = 0;
+            for (int row = 1; row < rowWidths.length; row++)
+            {
+                if (rowWidths[row] < rowWidths[minWidthIndex])
+                {
+                    minWidthIndex = row;
+                }
+            }
+            int childHeight = MainActivity.getDisplayMetricsController().getScreenHeight() / rowWidths.length;
+            childRect.left = rowWidths[minWidthIndex];
+            childRect.top = childHeight * minWidthIndex % MainActivity.getDisplayMetricsController().getScreenHeight();
+            childRect.right = childRect.left + child.getMeasuredWidth();
+            childRect.bottom = childRect.top + childHeight;
+            getChildAt(i).layout(childRect.left, childRect.top, childRect.right, childRect.bottom);
+            rowWidths[minWidthIndex] += child.getMeasuredWidth();
         }
     }
 }
