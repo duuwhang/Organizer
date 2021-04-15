e6ef50f53103cf0a59a7fc4f9badfe69a4ba6079
duuwhang <maxipulstorm@aon.at>
2021-04-15 20:08:07 +0200

    add gray add layout background

--- app/src/main/java/com/organizer/layouts/add/AddTaskLayout.java
+++ app/src/main/java/com/organizer/layouts/add/AddTaskLayout.java
@@ -1,7 +1,8 @@
 package com.organizer.layouts.add;
 
 import android.content.Context;
-import android.widget.Button;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import com.organizer.layouts.BaseLayout;
 
 public class AddTaskLayout extends BaseLayout
@@ -9,8 +10,9 @@ public class AddTaskLayout extends BaseLayout
     public AddTaskLayout(Context context)
     {
         super(context);
-        Button button = new Button(context);
-        button.setText("task");
-        addView(button);
+        
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setTint(Color.DKGRAY);
+        setBackground(drawable);
     }
 }
