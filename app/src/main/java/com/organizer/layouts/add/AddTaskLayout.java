6a8cf685f6bf4bf849c245a900df5a49370832a4
duuwhang <maxipulstorm@aon.at>
2021-04-20 08:03:13 +0200

    implement title input field

--- app/src/main/java/com/organizer/layouts/add/AddTaskLayout.java
+++ app/src/main/java/com/organizer/layouts/add/AddTaskLayout.java
@@ -4,14 +4,20 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.GradientDrawable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import com.organizer.MainActivity;
+import com.organizer.R;
 import com.organizer.layouts.BaseLayout;
 
 public class AddTaskLayout extends BaseLayout
 {
+    private final int defaultHintColor;
     private Button addButton;
+    private EditText titleEditText;
     private final Rect childRect = new Rect();
     
     public AddTaskLayout(Context context)
@@ -29,11 +35,45 @@ public class AddTaskLayout extends BaseLayout
             @Override
             public void onClick(View v)
             {
-                MainActivity.getInstance().getLayout().getToDoLayout().addTask("Add Tasks");
-                MainActivity.getInstance().getLayout().toggleAddLayout(false);
+                if (!titleEditText.getText().toString().equals(""))
+                {
+                    MainActivity.getInstance().getLayout().getToDoLayout().addTask(titleEditText.getText().toString());
+                    MainActivity.getInstance().getLayout().toggleAddLayout(false);
+                    titleEditText.setText("");
+                    titleEditText.setHintTextColor(defaultHintColor);
+                    titleEditText.clearFocus();
+                }
+                else
+                {
+                    titleEditText.setHintTextColor(getResources().getColor(R.color.colorAccent));
+                }
             }
         });
         addView(addButton);
+        
+        titleEditText = new EditText(context);
+        defaultHintColor = titleEditText.getHintTextColors().getDefaultColor();
+        titleEditText.setHint("Title");
+        titleEditText.setCursorVisible(false);
+        titleEditText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+            }
+            
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                titleEditText.setHintTextColor(defaultHintColor);
+            }
+            
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+            }
+        });
+        addView(titleEditText);
     }
     
     @Override
@@ -42,16 +82,18 @@ public class AddTaskLayout extends BaseLayout
         int width = right - left;
         int height = bottom - top;
         
-        for (int i = 1; i < getChildCount(); i++)
-        {
-            getChildAt(i).layout(0, 0, width, height);
-        }
-        
         int margin = MainActivity.getDisplayMetricsController().dpToPx(6);
         childRect.left = width - addButton.getMeasuredWidth() - margin;
         childRect.top = height - addButton.getMeasuredHeight() - margin;
         childRect.right = width - margin;
         childRect.bottom = height - margin;
         addButton.layout(childRect.left, childRect.top, childRect.right, childRect.bottom);
+        
+        height = childRect.top;
+        
+        for (int i = 1; i < getChildCount(); i++)
+        {
+            getChildAt(i).layout(0, 0, width, height);
+        }
     }
 }
